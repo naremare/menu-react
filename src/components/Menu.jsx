f54ce22a4789eb930b863884c352e3b102252be6
naremare <nare.sarksyan@gmail.com>
2023-03-05 13:43:59 +0100

    added description

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,28 +10,33 @@ const Menu = () => {
     <div className="menu">
       <h1 className="menu-title">Ресторан "Нелли"</h1>
       <div className="main">
-        <div className="titles" >
+        <div className="titles">
           {Object.keys(data).map((category) => (
-            <button  key={category} className="menu-subtitle" onClick={(e) => setItem(data[category])}>
+            <button
+              key={category}
+              className="menu-subtitle"
+              onClick={(e) => setItem(data[category])}
+            >
               {category}
             </button>
           ))}
         </div>
         <div className="items">
           <ul className="category_item">
-            {
-              item.map((i) => (
-                <li className="title_item">
+            {item.map((i) => (
+              <li className="title_item">
+                <div className="item_name">
                   <span>{i.name}</span>
-                  <span>{i.price}</span>
-                </li>
-              ))
-            }    
+                  <span className="description">{i.description}</span>
+                </div>
+                <span>{i.price}</span>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Menu
