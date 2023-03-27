5a2ddb6504cc9b8876e9649f8bef8e9d27108888
naremare <nare.sarksyan@gmail.com>
2023-03-27 11:59:49 +0200

    NavBar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import Menu from './components/Menu';
-import image from './assets/image.WEBP.jpeg'
+import "./App.css";
+import Menu from "./components/Menu/Menu";
+import NavBar from "./components/NavBar/NavBar";
+import image from "./assets/image.WEBP.jpeg";
 
 function App() {
   return (
-    <div className="App" style={{ height: '100vh', backgroundImage:`url(${image})`, backgroundRepeat:"no-repeat", backgroundSize:"cover" }}>
-      <div className='bg-color'>
-        <Menu />
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        height: "100dvh",
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="bg-color">
+        <div className="menu">
+          {/* <h1 className="menu-title">Ресторан "Нелли"</h1> */}
+          <NavBar />
+          <Menu />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <div className="nav">
+            <a href="">Меню</a>
+            <a href="">Галлерея</a>
+            <a href="">Бронирование</a>
+        </div>
+    )
+}
+
+export default NavBar;
+
