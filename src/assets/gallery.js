7b03aec693813ca938dc38bab62f341dc2f3c7d1
Tigran <41443306+septoon@users.noreply.github.com>
2023-03-28 14:54:01 +0300

    added gallery data

---
+++ src/assets/gallery.js
@@ -0,0 +1,47 @@
+export const gallery = [
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4321783/2a000001825a5a5280ed14db6863b8b3c15d/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/3569559/2a000001825a594d6538835f7060da899024/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4255939/2a000001825a586f7ed8559031c03f02bd0d/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4449685/2a000001825a58e4fa830bdbf0aa7de05729/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/5272526/2a000001825a5e4467e00c972b7e6fb9b159/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4562252/2a000001825a5dc4c37be0cbdb90f90f3737/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/6159907/2a000001825a5e706c5b3e029d3bb10a7135/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/5104371/2a000001825a5c68248c8c746ba09f733049/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/5298780/2a000001825a5d97e5ee8e22670fdbd196a8/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/3598710/2a000001825a5d48cb3ce7b69a1b974f017f/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4265086/2a000001825a5b07a828d482b12ec88b8638/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4476499/2a000001825a5df4578506f1abe50535c0cc/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4392922/2a000001825a5cd84d86bb427f1290a54f22/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4449685/2a000001825a5c7d5ecf37e0f2917a8b98e6/XXXL"
+  },
+  {
+    "img": "https://avatars.mds.yandex.net/get-altay/4012790/2a000001825a5ba3fe5ee8f66188396ce6dc/XXXL"
+  }
+]
