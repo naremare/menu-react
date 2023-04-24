61fcd524d69c4904584058c49c78854a976414bf
naremare <nare.sarksyan@gmail.com>
2023-04-24 13:49:56 +0200

    grid updates

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -6,7 +6,12 @@ const Booking = () => {
   return (
     <div className="booking">
       <div className="contacts">
-        Наши контакты
+        <h1>Наши контакты</h1><br />
+        <address>ул. Ленина, 9Б, г. Алушта</address><br />
+        <p>Мы открыты:</p>
+        <p>11:00 - 23:00</p><br />
+        <p>Забронируйте столик по номеру: 
+        <br /><a href="tel:79785548181">+7 (978) 554-81-81</a></p>
       </div>
       <div className="map-wrapper">
         <Maps />
