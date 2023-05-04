c47103ed07ac6a5d68ea4c7533c5eff2f8938179
naremare <nare.sarksyan@gmail.com>
2023-05-04 18:22:25 +0200

    home copmopnent update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import image from "./assets/images/IMG_8303.WEBP";
 import { Route, Routes } from "react-router-dom";
 import Gallery from "./components/Gallery/Gallery";
 import Booking from "./components/Booking/Booking";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
@@ -20,13 +21,15 @@ function App() {
     >
       <div className="bg-color">
         <div className="menu">
-          {/* <h1 className="menu-title">Ресторан "Нелли"</h1> */}
-          <NavBar />
-          <Routes>
-            <Route exact path='/menu-react' element={<Menu />} />
-            <Route path='/gallery' element={<Gallery />} />
-            <Route path='/booking' element={<Booking />} />
-          </Routes>
+          <div>
+            <NavBar />
+            <Routes>
+              <Route exact path="/home" element={<Home />} />
+              <Route path="/menu-react" element={<Menu />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/booking" element={<Booking />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div>
+        <h1>
+          Добро пожаловать в ресторан <span className="name">"Нелли"</span>
+        </h1>
+        <p className="cursive">Вкусно есть не запретишь!</p>
+        <div className="flex-items">
+          <p className="flex-item">
+            <a href="./Menu" className="link">Меню</a>
+          </p>
+          <p className="flex-item">
+            <a href="./Booking" className="link">Забронировать</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );};
+
+export default Home
